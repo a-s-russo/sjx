1f662f14edb73b8e0645e0aea4dfd5ae93c4502e
Anthony Russo <a.s.russo@outlook.com>
2023-10-30 16:08:47 +1030

    Create pipeline

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# time-series-comparison
+# sjx
 Comparison of X-13ARIMA-SEATS and JDemetra+ seasonal adjustment methods on ABS data
+
+# How to run
+
+- Clone the repository: `git clone git@github.com:a-s-russo/sjx.git`.
+- Switch to the `pipeline` branch: `git checkout pipeline`.
+- Start an R session in the folder and run `renv::restore()`
+   to install the project’s dependencies.
+- Run the pipeline with `targets::tar_make()`.
+- Checkout `sa_method_comparison.html` for the output.
---
+++ _targets.R
@@ -0,0 +1,21 @@
+library(targets)
+library(tarchetypes)
+
+tar_option_set(packages = "sjx")
+
+read_data <- function(data_name, package_name) {
+  temp <- new.env(parent = emptyenv())
+  
+  data(list = data_name,
+       package = package_name,
+       envir = temp)
+  
+  get(data_name, envir = temp)
+}
+
+list(
+  tar_target(inventories,
+             read_data("inventories", "sjx")),
+  tar_target(sales, read_data("sales", "sjx")),
+  tar_render(analyse, "sa_method_comparison.Rmd")
+)
